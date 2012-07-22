3d55397a27158591332ab42b9ad4b00c632b73ce
pvaleso <amigotli@hotmail.com>
2012-07-21 20:34:16 -0500

    first commit

---
+++ config.ru
@@ -0,0 +1,5 @@
+# This file is used by Rack-based servers to start the application.
+
+require ::File.expand_path('../config/environment',  __FILE__)
+## run RackOauth2Sample::Application
+run Caravana::Application
---
+++ config/environment.rb
@@ -0,0 +1,6 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+## RackOauth2Sample::Application.initialize!
+Caravana::Application.initialize!
